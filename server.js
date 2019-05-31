38fd63214191055290d0cf0cda8f58c416295bfa
Carlosf96 <carlos.f.roque96@gmail.com>
2019-05-31 15:23:35 -0500

    Refactored backened to use async await

--- server.js
+++ server.js
@@ -1,47 +1,54 @@
 //find and use technologies we will use.
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const users = require('./routes/api/users');
-const app = express();
-const path = require('path');
-//bodyparse middleware
-app.use(bodyParser.urlencoded({
-    extended: false
-})); //parse the url
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
+const path = require("path");
 
-app.use(bodyParser.json());//parse to jason
+(async () => {
+	try {
+		await mongoose.connect(db, { useNewUrlParser: true }) //our connection to mongoDB
+		console.log("MongoDB connected successfully!")
 
-const db = require("./config/keys").mongoURI;//require config for DB
+		const app = express();
+		app.disable("x-powered-by");
+		//bodyparse middleware
+		app.use(
+			bodyParser.urlencoded({
+				extended: false
+			})
+		); //parse the url
 
-mongoose//our connection to mongoDB
-    .connect(
-        db, {
-            useNewUrlParser: true
-        })
-    .then(() => console.log("MongoDB connected successfully!"))
-    .catch(err => console.log(err))
-//passport middleware
-app.use(passport.initialize());
-//passport configs
-require('./config/passport')(passport);
-//routes
-app.use('/api/users', users)
-//server static assets if in production 
-if (process.env.NODE_ENV === 'production') {
-   //set a static folder
-  app.use(express.static('client/build')); 
-  //app.use(express.static(path.join(__dirname, 'client/build')));
+		app.use(bodyParser.json()); //parse to jason
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
-const port = process.env.PORT || 5000; 
+		const db = require("./config/keys").mongoURI; //require config for DB
 
 
-app.listen(port, "0.0.0.0", () => {//let server listen on defined port
-    console.log(`App listening on ${port}!`);
-});
+		//passport middleware
+		app.use(passport.initialize());
+		//passport configs
+		require("./config/passport")(passport);
+		//routes
+		app.use("/api/users", users);
+		//server static assets if in production
+		if (process.env.NODE_ENV === "production") {
+			//set a static folder
+			app.use(express.static("client/build"));
+			//app.use(express.static(path.join(__dirname, 'client/build')));
+
+			app.get("*", (req, res) => {
+				res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+			});
+		}
+
+		const port = process.env.PORT || 5000;
+
+		app.listen(port, "0.0.0.0", () => {
+			//let server listen on defined port
+			console.log(`App listening on ${port}!`);
+		})
+	} catch (err) {
+		console.log(err)
+	}
+})();
